bc33ddaf6a9995221cf51d231d726b7401c70271
ali27-dev <khanali3229@gmail.com>
2025-06-17 11:45:38 +0500

    change api-key and generate new one and aslo i try to implement the funtionality of remove upload-recipe but did't it, i will implement this functonality as soon as possible

--- forkify-project/src/js/config.js
+++ forkify-project/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'fcda291f-14b7-445c-84d6-5363664ed1b3';
+export const KEY = 'f6de4ddc-69b1-4d1b-bff2-a43137f9365f';
 export const MODAL_CLOSE_SEC = 2.5;
